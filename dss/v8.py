f51680e9b059296fdd8d60a41a4ca8991a32f63c
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2018-11-03 23:37:57 -0300

    Remove v7 and v8 folders; use simple modules instead.

--- dss/v8.py
+++ dss/v8.py
@@ -3,10 +3,10 @@
 Copyright (c) 2016-2018 Paulo Meira
 '''
 from __future__ import absolute_import
-from .._dss_capi_v8 import ffi, lib
-from .._cffi_api_util import CffiApiUtil, use_com_compat
-from .. import dss_capi, dss_capi_alt, enums
-from ..enums import *
+from ._dss_capi_v8 import ffi, lib
+from ._cffi_api_util import CffiApiUtil, use_com_compat
+from . import dss_capi, dss_capi_alt, enums
+from .enums import *
 
 # Bind to the FFI module instance for OpenDSS-v8
 api_util = CffiApiUtil(ffi, lib)
