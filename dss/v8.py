03f533858169f0eea63558a3c69bb698aaccb845
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2020-03-01 23:49:01 -0300

    Disable v8 for the moment. Please open an issue if you're a user that needs it right now.

--- dss/v8.py
+++ dss/v8.py
@@ -1,17 +1,8 @@
 '''
-This module presents instances for the OpenDSS Version 8 implementation. It is the parallel-machine version, which allow creating multiple solvers (actors) in a single process. Compared to Version 7, it is less tested, but feel free to open an issue on GitHub if you find any problem.
+This module used to present an instance for the OpenDSS Version 8 implementation. It was based on the official parallel-machine version, which allow creating multiple solvers (actors) in a single process. The mechanism is being reword in DSS C-API, the library used by DSS_Python to implement OpenDSS, to reproduce a stable version in the future.
 '''
 from __future__ import absolute_import
-from ._dss_capi_v8 import ffi, lib
-from ._cffi_api_util import CffiApiUtil, use_com_compat, DssException
-from . import dss_capi_gr, dss_capi_ir, enums
-from .enums import *
+from .v7 import *
 
-# Bind to the FFI module instance for OpenDSS-v8
-api_util = CffiApiUtil(ffi, lib) #: API utility functions and low-level access for the Version 8 library
+warnings.warn('This module (dss.v8) is deprecated. Most parallel-machine functions are planned to be integrated in a future version.')
 
-DSS_GR = dss_capi_gr.IDSS(api_util) #: GR (Global Result) interface to the Version 8 library
-DSS_IR = dss_capi_ir.IDSS(api_util) #: IR (Immediate Result) interface to the Version 8 library
-DSS = DSS_GR #: Same as DSS_GR
-
-#__all__ = ['DSS', 'DSS_GR', 'DSS_IR', 'api_util']
