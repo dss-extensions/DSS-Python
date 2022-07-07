dcc125a8d4a7d2363945dd3da426b41e10a1d0e2
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2022-07-07 03:51:56 -0300

    ZIP: add docstrings

--- dss/dss_capi_gr/IZIP.py
+++ dss/dss_capi_gr/IZIP.py
@@ -11,15 +11,32 @@ class IZIP(Base):
     _columns = []
 
     def Open(self, Value):
+        '''
+        Opens and prepares a ZIP file to be used by the DSS text parser
+        
+        (API Extension)
+        '''
         if type(Value) is not bytes:
             Value = Value.encode(self._api_util.codec)
 
         self.CheckForError(self._lib.ZIP_Open(Value))
 
     def Close(self):
+        '''
+        Closes the current open ZIP file
+        
+        (API Extension)
+        '''
         self.CheckForError(self._lib.ZIP_Close())
 
     def Redirect(self, Value):
+        '''
+        Runs a "Redirect" command inside the current (open) ZIP file.
+        In the current implementation, all files required by the script must
+        be present inside the ZIP, using relative paths.
+
+        (API Extension)
+        '''
         if type(Value) is not bytes:
             Value = Value.encode(self._api_util.codec)
 
