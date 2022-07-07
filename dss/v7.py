33ca19310321f41351a261dc576f2b1b574d7838
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2022-07-07 03:51:56 -0300

    Adjust comments and imports

--- dss/v7.py
+++ dss/v7.py
@@ -12,25 +12,24 @@ else:
     warnings.warn('Environment variable DSS_EXTENSIONS_DEBUG=1 is set: loading the debug version of the DSS C-API library')
     from ._dss_capid import ffi, lib
 
-from ._cffi_api_util import CffiApiUtil, use_com_compat, DSSException
+from ._cffi_api_util import CffiApiUtil, set_case_insensitive_attributes, use_com_compat, DSSException
 from . import dss_capi_gr, dss_capi_ir, enums
 from .enums import *
 
 DssException = DSSException
 
 # Bind to the FFI module instance for OpenDSS-v7
-api_util = CffiApiUtil(ffi, lib) #: API utility functions and low-level access for the Version 7 library
+api_util = CffiApiUtil(ffi, lib) #: API utility functions and low-level access to the classic API
 
 if not hasattr(lib, 'ctx_New'):
     # Module was built without the context API
     prime_api_util = None
-    DSS_GR = dss_capi_gr.IDSS(api_util) #: GR (Global Result) interface to the Version 7 library
-    DSS_IR = dss_capi_ir.IDSS(api_util) #: IR (Immediate Result) interface to the Version 7 library
+    DSS_GR = dss_capi_gr.IDSS(api_util) #: GR (Global Result) interface
+    DSS_IR = dss_capi_ir.IDSS(api_util) #: IR (Immediate Result) interface
 else:
-    prime_api_util = CffiApiUtil(ffi, lib, lib.ctx_Get_Prime()) #: API utility functions and low-level access for the Version 7 library
+    prime_api_util = CffiApiUtil(ffi, lib, lib.ctx_Get_Prime()) #: API utility functions and low-level access for DSSContext API
     DSS_GR = dss_capi_gr.IDSS(prime_api_util) #: GR (Global Result) interface using the new DSSContext API
     DSS_IR = dss_capi_ir.IDSS(prime_api_util) #: IR (Immediate Result) interface using the new DSSContext API
 
-DSS = DSS_GR #: Same as DSS_GR
-
-#__all__ = ['DSS', 'DSS_GR', 'DSS_IR', 'api_util']
+# Added "dss" for v0.12+ (feedback from some users)
+dss = DSS = DSS_GR #: Same as DSS_GR
