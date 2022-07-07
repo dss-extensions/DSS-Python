62dff365ee21c8b5ddd43a46cb07f1c235d6542a
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2022-07-07 03:54:04 -0300

    ZIP: expose Contains and List.
    Some dunder methods were also added.

--- dss/dss_capi_gr/IZIP.py
+++ dss/dss_capi_gr/IZIP.py
@@ -12,7 +12,10 @@ class IZIP(Base):
 
     def Open(self, Value):
         '''
-        Opens and prepares a ZIP file to be used by the DSS text parser
+        Opens and prepares a ZIP file to be used by the DSS text parser.
+        Currently, the ZIP format support is limited by what is provided in the Free Pascal distribution.
+        Besides that, the full filenames inside the ZIP must be shorter than 256 characters.
+        The limitations should be removed in a future revision.
         
         (API Extension)
         '''
@@ -33,7 +36,8 @@ class IZIP(Base):
         '''
         Runs a "Redirect" command inside the current (open) ZIP file.
         In the current implementation, all files required by the script must
-        be present inside the ZIP, using relative paths.
+        be present inside the ZIP, using relative paths. The only exceptions are
+        memory-mapped files.
 
         (API Extension)
         '''
@@ -56,4 +60,39 @@ class IZIP(Base):
         self.CheckForError(self._lib.ZIP_Extract_GR(FileName))
         ptr, cnt = api_util.gr_int8_pointers
         return bytes(api_util.ffi.buffer(ptr[0], cnt[0]))
+
+    def List(self, regexp=None):
+        '''
+        List of strings consisting of all names match the regular expression provided in regexp.
+        If no expression is provided, all names in the current open ZIP are returned.
+        
+        See https://regex.sorokin.engineer/en/latest/regular_expressions.html for information on 
+        the expression syntax and options.
+
+        (API Extension)
+        '''
+        if regexp is None or not regexp:
+            regexp = self._api_util.ffi.NULL
+        else:
+            if type(regexp) is not bytes:
+                regexp = regexp.encode(self._api_util.codec)
+        
+        return self.CheckForError(self._get_string_array(self._lib.ZIP_List, regexp))
+
+    def Contains(self, Name):
+        '''
+        Check if the given path name is present in the current ZIP file.
+        
+        (API Extension)
+        '''
+        if type(Name) is not bytes:
+            Name = Name.encode(self._api_util.codec)
+
+        return self.CheckForError(self._lib.ZIP_Contains(Name)) != 0
+
+    def __getitem__(self, FileName):
+        return self.Extract(FileName)
+
+    def __contains__(self, Name):
+        return self.Contains(Name)
 
