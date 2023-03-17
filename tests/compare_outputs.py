2813e852b1ce6350ff1493ed7d198ab524a5245f
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2023-03-17 10:13:18 -0300

    Tests: add/sort the known diffs; initial XML diff.

--- tests/compare_outputs.py
+++ tests/compare_outputs.py
@@ -1,10 +1,11 @@
-import json, os
+import json, re
 from inspect import getdoc
 from collections import defaultdict
 from zipfile import ZipFile, BadZipFile
 import numpy as np
 from pprint import pprint
 from dss import ICircuit, IDSS
+import xmldiff.main, xmldiff.actions
 
 np.set_printoptions(linewidth=300)
 
@@ -17,6 +18,7 @@ class MISSING:
 tol = 1e-5
 
 VERBOSE = False
+ENABLE_XML = False
 
 KNOWN_COM_DIFF = set([
     # On official COM, uninitialized values for CalcCurrent, AllocFactors
@@ -27,48 +29,51 @@ KNOWN_COM_DIFF = set([
     *[('Version8/Distrib/Examples/DOCTechNote/1_2.dss.json', 'Meters', 'records', x, 'AllocFactors') for x in range(77)],
     *[('Version8/Distrib/Examples/DOCTechNote/2_1.dss.json', 'Meters', 'records', x, 'AllocFactors') for x in range(77)],
     *[('Version8/Distrib/Examples/DOCTechNote/2_2.dss.json', 'Meters', 'records', x, 'AllocFactors') for x in range(77)],
-    ('Version8/Distrib/IEEETestCases/123Bus/SolarRamp.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
-    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
+    ('Version8/Distrib/Examples/IBRDynamics_Cases/GFL_IEEE123/Run_IEEE123Bus_GFLDaily.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
+    ('Version8/Distrib/Examples/IBRDynamics_Cases/GFM_IEEE123/Run_IEEE123Bus_GFMDaily.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
     ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE123/Run_IEEE123Bus_GFMSnap-A.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
-    ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE123/Run_IEEE123Bus_GFMSnap-C.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
-    ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE123/Run_IEEE123Bus_GFMWholeDaily.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
-    ('Version8/Distrib/Examples/Scripts/IEEE-TIA-LV Model/master_large_1phase.dss.json', 'Meters', 'records', 0, 'CalcCurrent'),
-    ('Version8/Distrib/Examples/Scripts/IEEE-TIA-LV Model/master_small_1phase.dss.json', 'Meters', 'records', 0, 'CalcCurrent'),
-    ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE123/Run_IEEE123Bus_GFMWholeDaily.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
     ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE123/Run_IEEE123Bus_GFMSnap-A.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
     ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE123/Run_IEEE123Bus_GFMSnap-B.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
     ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE123/Run_IEEE123Bus_GFMSnap-C.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
-    ('Version8/Distrib/Examples/IBRDynamics_Cases/GFM_IEEE123/Run_IEEE123Bus_GFMDaily.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
-    ('Version8/Distrib/Examples/IBRDynamics_Cases/GFL_IEEE123/Run_IEEE123Bus_GFLDaily.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
+    ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE123/Run_IEEE123Bus_GFMSnap-C.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
+    ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE123/Run_IEEE123Bus_GFMWholeDaily.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
+    ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE123/Run_IEEE123Bus_GFMWholeDaily.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
+    ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE8500/Run_8500Node_GFMSnap.dss.json', 'Meters', 'records', 0, 'AllocFactors'),
+    ('Version8/Distrib/Examples/Scripts/IEEE-TIA-LV Model/master_large_1phase.dss.json', 'Meters', 'records', 0, 'CalcCurrent'),
+    ('Version8/Distrib/Examples/Scripts/IEEE-TIA-LV Model/master_small_1phase.dss.json', 'Meters', 'records', 0, 'CalcCurrent'),
+    ('Version8/Distrib/IEEETestCases/123Bus/SolarRamp.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
+    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Meters', 'records', 0, 'AllocFactors'),
 
     # Close enough for the system, especially since everything else matches
     # Some of these are due to tiny line segments, others various sources
     ('Test/AutoTrans/Auto3bus.dss.json', 'Lines', 'records', 1, 'ActiveCktElement', 'Residuals'),
     ('Test/AutoTrans/AutoHLT.dss.json', 'Lines', 'records', 0, 'ActiveCktElement', 'Residuals'),
-    ('Version8/Distrib/Examples/InverterModels/PVSystem/InvControl/VV_VW/Daily_VVVW_varMax_PMPPPU_kVAlimitation_Qpriority-2.dss.json', 'PVSystems', 'records', 0, 'RegisterValues'),
-    ('Version8/Distrib/EPRITestCircuits/epri_dpv/M1/Master_NoPV.dss.json', 'Lines', 'records', 235, 'ActiveCktElement', 'Losses'),
-    ('Version8/Distrib/EPRITestCircuits/epri_dpv/M1/Master_NoPV.dss.json', 'Lines', 'records', 332, 'ActiveCktElement', 'Losses'),
-    ('Version8/Distrib/EPRITestCircuits/epri_dpv/M1/Master_NoPV.dss.json', 'Lines', 'records', 395, 'ActiveCktElement', 'Losses'),
-    ('Version8/Distrib/EPRITestCircuits/epri_dpv/M1/Master_NoPV.dss.json', 'Lines', 'records', 398, 'ActiveCktElement', 'Losses'),
-    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'Currents'),
-    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'SeqCurrents'),
-    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'CplxSeqCurrents'),
-    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'Powers'),
-    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'CurrentsMagAng'),
-    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'Residuals'),
-    ('Version8/Distrib/EPRITestCircuits/ckt5/Master_ckt5.dss.json', 'Lines', 'records', 99, 'ActiveCktElement', 'Powers'),
+    ('Version8/Distrib/EPRITestCircuits/ckt24/master_ckt24.dss.json', 'Lines', 'records', 487, 'ActiveCktElement', 'Losses'),
+    ('Version8/Distrib/EPRITestCircuits/ckt24/master_ckt24.dss.json', 'Lines', 'records', 5221, 'ActiveCktElement', 'Losses'),
+    ('Version8/Distrib/EPRITestCircuits/ckt24/master_ckt24.dss.json', 'Lines', 'records', 5221, 'ActiveCktElement', 'PhaseLosses'),
     ('Version8/Distrib/EPRITestCircuits/ckt5/Master_ckt5.dss.json', 'Lines', 'records', 109, 'ActiveCktElement', 'Powers'),
     ('Version8/Distrib/EPRITestCircuits/ckt5/Master_ckt5.dss.json', 'Lines', 'records', 708, 'ActiveCktElement', 'Powers'),
+    ('Version8/Distrib/EPRITestCircuits/ckt5/Master_ckt5.dss.json', 'Lines', 'records', 99, 'ActiveCktElement', 'Powers'),
     ('Version8/Distrib/EPRITestCircuits/ckt7/Master_ckt7.dss.json', 'Lines', 'records', 164, 'ActiveCktElement', 'Losses'),
     ('Version8/Distrib/EPRITestCircuits/ckt7/Master_ckt7.dss.json', 'Lines', 'records', 245, 'ActiveCktElement', 'Losses'),
     ('Version8/Distrib/EPRITestCircuits/ckt7/Master_ckt7.dss.json', 'Lines', 'records', 262, 'ActiveCktElement', 'Losses'),
-    ('Version8/Distrib/EPRITestCircuits/ckt24/master_ckt24.dss.json', 'Lines', 'records', 487, 'ActiveCktElement', 'Losses'),
-    ('Version8/Distrib/EPRITestCircuits/ckt24/master_ckt24.dss.json', 'Lines', 'records', 5221, 'ActiveCktElement', 'PhaseLosses'),
-    ('Version8/Distrib/EPRITestCircuits/ckt24/master_ckt24.dss.json', 'Lines', 'records', 5221, 'ActiveCktElement', 'Losses'),
+    ('Version8/Distrib/EPRITestCircuits/epri_dpv/M1/Master_NoPV.dss.json', 'Lines', 'records', 235, 'ActiveCktElement', 'Losses'),
+    ('Version8/Distrib/EPRITestCircuits/epri_dpv/M1/Master_NoPV.dss.json', 'Lines', 'records', 308, 'ActiveCktElement', 'Losses'),
+    ('Version8/Distrib/EPRITestCircuits/epri_dpv/M1/Master_NoPV.dss.json', 'Lines', 'records', 332, 'ActiveCktElement', 'Losses'),
+    ('Version8/Distrib/EPRITestCircuits/epri_dpv/M1/Master_NoPV.dss.json', 'Lines', 'records', 395, 'ActiveCktElement', 'Losses'),
+    ('Version8/Distrib/EPRITestCircuits/epri_dpv/M1/Master_NoPV.dss.json', 'Lines', 'records', 398, 'ActiveCktElement', 'Losses'),
+    ('Version8/Distrib/Examples/InverterModels/PVSystem/InvControl/VV_VW/Daily_VVVW_varMax_PMPPPU_kVAlimitation_Qpriority-2.dss.json', 'PVSystems', 'records', 0, 'RegisterValues'),
+    ('Version8/Distrib/Examples/Microgrid/GridFormingInverter/GFM_IEEE8500/Run_8500Node_Unbal.dss.json', 'Lines', 'records', 2029, 'ActiveCktElement', 'Losses'),
     ('Version8/Distrib/Examples/StorageControllerTechNote/LoadShape/LoadShapeRun.dss.json', 'Lines', 'records', 2029, 'ActiveCktElement', 'Losses'),
     ('Version8/Distrib/Examples/StorageControllerTechNote/PeakShave/PeakShaveMonPhaseRun.dss.json', 'Lines', 'records', 2029, 'ActiveCktElement', 'Losses'),
     ('Version8/Distrib/Examples/StorageControllerTechNote/PeakShaveDch_PeakShaveLow_Ch/PeakShaveDch_PeakShaveLow_ChRun.dss.json', 'Lines', 'records', 2029, 'ActiveCktElement', 'Losses'),
-    ('Version8/Distrib/Examples/StorageControllerTechNote/Support/SupportRun.dss.json', 'Lines', 'records', 2029, 'ActiveCktElement', 'Losses')
+    ('Version8/Distrib/Examples/StorageControllerTechNote/Support/SupportRun.dss.json', 'Lines', 'records', 2029, 'ActiveCktElement', 'Losses'),
+    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'CplxSeqCurrents'),
+    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'Currents'),
+    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'CurrentsMagAng'),
+    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'Powers'),
+    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'Residuals'),
+    ('Version8/Distrib/IEEETestCases/SecondaryTestCircuit_modified/Master.DSS.json', 'Lines', 'records', 0, 'ActiveCktElement', 'SeqCurrents'),
 ])
 
 
@@ -307,22 +312,12 @@ class ComparisonHandler:
             self.element_compare(va, vb, path)
 
 
-    def split(self):
+    def splitter(self):
         with ZipFile(self.fnA, 'r') as zipA:
             filenames = [finfo.filename for finfo in zipA.filelist]
 
-        step = len(filenames) // os.cpu_count()
-        parts = []
-        last_proc = os.cpu_count()
-        for i in range(last_proc + 1):
-            i_start = i * step
-            i_last = (i + 1) * step
-            if i == last_proc and i_last != len(filenames):
-                i_last = len(filenames)
-
-            parts.append((self.fnA, self.fnB, filenames[i_start : i_last], VERBOSE))
-        
-        return parts
+        for fn in filenames:
+            yield (self.fnA, self.fnB, fn, VERBOSE)
 
 
     def merge(self, results):
@@ -333,9 +328,6 @@ class ComparisonHandler:
 
 
     def compare_all(self):
-        print(self.fnA)
-        print(self.fnB)
-
         with ZipFile(self.fnA, 'r') as zipA, ZipFile(self.fnB, 'r') as zipB:
             self.total = 0
             if self.fns:
@@ -344,7 +336,6 @@ class ComparisonHandler:
                 filenames = [finfo.filename for finfo in zipA.filelist]
 
             for fn in filenames:
-                VERBOSE = True
                 try:
                     fB = zipB.open(fn, 'r')
                     fA = zipA.open(fn, 'r')
@@ -356,28 +347,56 @@ class ComparisonHandler:
                     print('BAD:', fn)
                     continue
 
-                dataA = json.load(fA)
-                dataB = json.load(fB)
+                if fn.endswith('.json'):
+                    dataA = json.load(fA)
+                    dataB = json.load(fB)
 
-                if not dataA['Solution']['Converged']:
-                    print('Skipping, not converged in the official version:', fn)
-                    continue
+                    if not dataA['Solution']['Converged']:
+                        print('Skipping, not converged in A:', fn)
+                        continue
 
-                self.A_IS_COM = 'C-API' not in dataA['DSS']['Version']
-                self.B_IS_COM = 'C-API' not in dataB['DSS']['Version']
-                try:
-                    self.compare(dataA, dataB, [fn])
-                except:
-                    print("COMPARE ERROR:", fn)
-                    raise
+                    self.A_IS_COM = 'C-API' not in dataA['DSS']['Version']
+                    self.B_IS_COM = 'C-API' not in dataB['DSS']['Version']
+                    try:
+                        self.compare(dataA, dataB, [fn])
+                    except:
+                        print("COMPARE ERROR:", fn)
+                        raise
+                
+                elif ENABLE_XML and fn.endswith('.xml'):
+                    # Comparing the XMLs is tricky, but this initial implementation 
+                    # can still be useful. For DSS C-API 0.13.0, we manually checked.
+                    # One issue is that the maintenance of ExportCIMXML.pas is not
+                    # in phase with OpenDSS releases, so we can't use the latest DLL
+                    # release for perfect comparison.
+                    textA = fA.read().decode()
+                    textB = fB.read().decode()
+
+                    # Float formatting is different (Delphi vs FPC). This removes
+                    # the extra zeros in the exponent part of floats (1E009 -> 1E9).
+                    # We could also just parse and reformat with Python if it becomes an
+                    # issue.
+                    textA = re.sub(r'(>[0-9\.]+E[+-]*)0+([^0]+<)', r'\1\2', textA)
+                    textB = re.sub(r'(>[0-9\.]+E[+-]*)0+([^0]+<)', r'\1\2', textB)
+
+                    diffs = xmldiff.main.diff_texts(textA.encode(), textB.encode())
+                    if diffs:
+                        for d in diffs:
+                            if isinstance(d, xmldiff.actions.MoveNode):
+                                continue
+
+                            print(type(d))
+                            
 
                 self.total += 1
 
 
-def compare_filelist(fnA, fnB, fns, verb):
+def compare_filelist(params):
+    fnA, fnB, fn, verb = params
+    
     global VERBOSE
     VERBOSE = verb
-    cmp = ComparisonHandler(fnA, fnB, fns)
+    cmp = ComparisonHandler(fnA, fnB, [fn])
     cmp.compare_all()
     return (dict(cmp.per_file), dict(cmp.per_file_detail), cmp.total)
 
@@ -392,10 +411,8 @@ if __name__ == '__main__':
     if mp:
         print("Using multiprocessing")
         from multiprocessing import Pool
-        parts = cmp.split()
         pool = Pool()
-        results = pool.starmap(compare_filelist, parts)
-        cmp.merge(results)
+        cmp.merge(pool.imap_unordered(compare_filelist, cmp.splitter()))
     else:
         cmp.compare_all()
 
@@ -408,7 +425,7 @@ if __name__ == '__main__':
     print()
     if not VERBOSE:
         for fn, cnt in cmp.per_file.items():
-            if cnt < 10:
+            if True:#cnt < 10:
                 print()
                 print(fn)
 
