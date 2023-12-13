de1332f66b6ea912c93c80b2fe9bdbe80d0dc504
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2023-12-13 03:55:38 -0300

    CI: install setuptools

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -1,5 +1,5 @@
 mkdir -p artifacts
 cd dss_python
-$PYTHON -m pip install --upgrade pip
+$PYTHON -m pip install --upgrade pip setuptools
 $PYTHON -m pip install cffi wheel
 $PYTHON setup.py --quiet bdist_wheel --py-limited-api cp37 --dist-dir=$ARTIFACTS_FOLDER
