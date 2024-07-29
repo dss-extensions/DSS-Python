121cb13ae5249361efefdadcfdad240b2cb3c783
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2024-07-28 22:17:49 -0300

    IDSSimComs: mark as deprecated

--- dss/IDSSimComs.py
+++ dss/IDSSimComs.py
@@ -3,14 +3,20 @@
 # Copyright (c) 2018-2024 DSS-Extensions contributors
 from ._cffi_api_util import Base
 from ._types import Float64Array
+import warnings
 
 class IDSSimComs(Base):
+    '''
+    **Deprecated**; use `DSS.ActiveCircuit.ActiveBus` API or the AltDSS alternatives instead
+    '''
     __slots__ = []
 
     def BusVoltage(self, Index: int) -> Float64Array:
+        warnings.warn('Use ActiveCircuit.ActiveBus or the AltDSS (AltDSS-Python) alternatives.', DeprecationWarning, stacklevel=2)
         return self._lib.DSSimComs_BusVoltage_GR(Index)
 
     def BusVoltagepu(self, Index: int) -> Float64Array:
+        warnings.warn('Use ActiveCircuit.ActiveBus or the AltDSS (AltDSS-Python) alternatives.', DeprecationWarning, stacklevel=2)
         return self._lib.DSSimComs_BusVoltagepu_GR(Index)
 
 
