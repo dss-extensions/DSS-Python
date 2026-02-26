525f192a4372e82f1edb36983312e473539d1c4f
Paulo Meira <10246101+PMeira@users.noreply.github.com>
2026-02-26 02:49:59 -0300

    CI: avoid virtualenv v21, hatch doesn't like it.

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -1,5 +1,5 @@
 mkdir -p artifacts
 cd dss_python
 $PYTHON -m pip install --upgrade pip wheel hatch
-$PYTHON -m pip install cffi numpy pytest
+$PYTHON -m pip install cffi numpy pytest 'virtualenv<21' 
 $PYTHON -m hatch build "$ARTIFACTS_FOLDER"
